cbe7a3539b526f7100a93d9ac8f0eccacb4bf889
Chukwuemezie Christopher Aneke <chukwuemezie.aneke.15@ucl.ac.uk>
2021-05-22 22:04:12 +0200

    write function to load page from url in form input

--- aboutpage.go
+++ aboutpage.go
@@ -4,14 +4,29 @@ import (
 	"html/template"
 	"net/http"
 
+	"io"
+
 	"github.com/ccaneke/webAnalytics/webAnalyzer"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "index", nil)
+func loadPage(url string) (io.Reader, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Body, nil
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *webAnalyzer.PageInformation) {
 	t, _ := template.ParseFiles(tmpl + ".html")
 	t.Execute(w, p)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index", nil)
+}
+
+func inputHandler(w http.ResponseWriter, r *http.Request) {
+
 }
