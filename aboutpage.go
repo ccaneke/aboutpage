8d872d8aa2bdabacd772a843655ae9db9de7ac93
Chukwuemezie Christopher Aneke <chukwuemezie.aneke.15@ucl.ac.uk>
2021-05-22 21:09:13 +0200

    write renderTemplate function, and add indexHandler

--- aboutpage.go
+++ aboutpage.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/ccaneke/webAnalytics/webAnalyzer"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *webAnalyzer.PageInformation) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ccaneke/aboutpage
 
 go 1.16
+
+require github.com/ccaneke/webAnalytics v0.0.0-20210521150043-7a22c0691cec
